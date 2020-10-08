e6de4cb17f03c35ee86caccfd239a202d9e97d12
paulo.esantos <paulo.esantos@gmail.com>
2020-10-08 19:19:38 -0300

    Criando Services

---
+++ src/services/TodoService.tsx
@@ -0,0 +1,13 @@
+import { Todo } from "../models/Todo";
+
+const TODO_STORE = 'todos';
+
+export const get = (): Todo[] => {
+    const data = localStorage.getItem(TODO_STORE) || '';
+    try {
+        const result = JSON.parse(data) as Todo[];
+        return result;
+    } catch {
+        return [];
+    }
+}
